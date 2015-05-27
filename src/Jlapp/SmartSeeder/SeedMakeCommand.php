c7a99961d24d321fa33ebbfd5f6443e7487d71ee
Jordan Lapp <jordanl@nutrie.com>
2015-05-26 17:59:11 -0700

    fixing config

--- src/Jlapp/SmartSeeder/SeedMakeCommand.php
+++ src/Jlapp/SmartSeeder/SeedMakeCommand.php
@@ -31,7 +31,7 @@ class SeedMakeCommand extends Command {
     public function fire()
     {
         $model = ucfirst($this->argument('model'));
-        $path = app_path(Config::get('smart-seeder::app.seedDir'));
+        $path = base_path(config('smart-seeder.seedDir'));
 
         $env = $this->option('env');
         if (!empty($env)) {
