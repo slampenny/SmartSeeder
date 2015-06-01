357aff03f84295a3141201cc33f2e9f4dc6931c2
May Pongpitpitak <may.pognpitpitak@aggregateiq.com>
2015-05-31 20:47:47 -0700

    Check in the environment specific folder
    Override the default `getMigrationFiles()` and `requireFiles()` to check
    not just the base folder as configured but also the folder named after
    the environment specified for seeding.

--- src/Jlapp/SmartSeeder/SeedMakeCommand.php
+++ src/Jlapp/SmartSeeder/SeedMakeCommand.php
@@ -49,7 +49,6 @@ class SeedMakeCommand extends Command {
 
         $fs = File::get(__DIR__."/stubs/DatabaseSeeder.stub");
 
-        $namespace = rtrim($this->getAppNamespace(), "/");
         $stub = str_replace('{{model}}', "seed_{$created}_".$model.'Seeder', $fs);
         $stub = str_replace('{{namespace}}', " namespace $namespace;", $stub);
         File::put($path, $stub);
