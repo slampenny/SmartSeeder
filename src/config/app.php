6b2dac9b80b86edc8163cb29d43274da29e77491
Jordan Ellinger <jordan@jordanellinger.com>
2015-06-03 21:42:57 -0700

    removed League File reference

--- src/config/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    /**
-     * DO NOT CHANGE THIS unless you also change the included migration, since this references the actual table in your database
-     */
-    'seedTable' => 'seeds',
-    'seedDir' => 'database/smartSeeds',
-);
